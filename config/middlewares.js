f8521372c75db456cf24a18478b6e665348e7e36
hipeac <hipeac.app@gmail.com>
2024-11-11 14:17:10 +0200

    Change cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
+module.exports.settings = {
+  cors: {
+    origin: [
+      "http://localhost:3000", // Your frontend URL (if you're testing locally)
+      "https://your-frontend-domain.com", // Your live frontend domain
+      "https://res.cloudinary.com", // Allow Cloudinary's domain
+    ],
+    headers: "*", // Allow all headers
+  },
+};
