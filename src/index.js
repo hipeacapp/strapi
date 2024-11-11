8441269a9deed633c14ffd8beccd585dba744601
hipeacapp <hipeac.app@gmail.com>
2024-11-11 12:47:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ module.exports = {
   bootstrap(/*{ strapi }*/) {
     setInterval(async () => {
       try {
-        await axios.get("https://hipeac-strapi.onrender.com/api/test");
+        await axios.get("https://hipeac-strapi.onrender.com/api/buildings");
         console.log("TEST!");
       } catch (error) {
         console.error("TEST failed", error.message);
