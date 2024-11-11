f31c1f7856b2511d05d944c15219d370c600ee50
hipeac <hipeac.app@gmail.com>
2024-11-11 13:38:23 +0200

    Add Cloudinary provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+// config/plugins.js
+
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"), // Your Cloudinary cloud name
+        api_key: env("CLOUDINARY_API_KEY"), // Your Cloudinary API key
+        api_secret: env("CLOUDINARY_API_SECRET"), // Your Cloudinary API secret
+      },
+    },
+  },
+});
