8b91af9335e0b34015e985c81e4a397023e0a3aa
hipeac <hipeac.app@gmail.com>
2024-11-11 14:38:03 +0200

    Allow images from Cloudinary - fix CSP issue again

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,26 @@
-module.exports = [
+module.exports = ({ env }) => [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          // Script sources (allow scripts from self, unsafe-inline)
+          "script-src": ["'self'", "'unsafe-inline'"],
+
+          // Image sources (allow images from self, data URIs, cdn.jsdelivr.net, strapi.io, AWS, and Cloudinary)
+          "img-src": [
+            "'self'",
+            "data:",
+            "strapi.io",
+            "https://res.cloudinary.com",
+          ],
+        },
+      },
+    },
+  },
+
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
@@ -10,18 +29,3 @@ module.exports = [
   "strapi::favicon",
   "strapi::public",
 ];
-
-module.exports.settings = {
-  security: {
-    contentSecurityPolicy: {
-      directives: {
-        // Allow images from your domain and Cloudinary
-        "img-src": ["'self'", "https://res.cloudinary.com"], // Add Cloudinary's domain
-        // You can add other resources here, like scripts and styles if needed
-        "default-src": ["'self'"], // Allow only the same origin for other resources
-        "script-src": ["'self'"], // Allow scripts only from the same origin
-        "style-src": ["'self'", "'unsafe-inline'"], // Allow styles from the same origin
-      },
-    },
-  },
-};
