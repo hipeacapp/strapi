19a53827ca13bbff043f58bbcab30939fec46baa
hipeac <hipeac.app@gmail.com>
2024-11-11 14:25:52 +0200

    Allow images from Cloudinary - fix CSP issue

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,3 +10,18 @@ module.exports = [
   "strapi::favicon",
   "strapi::public",
 ];
+
+module.exports.settings = {
+  security: {
+    contentSecurityPolicy: {
+      directives: {
+        // Allow images from your domain and Cloudinary
+        "img-src": ["'self'", "https://res.cloudinary.com"], // Add Cloudinary's domain
+        // You can add other resources here, like scripts and styles if needed
+        "default-src": ["'self'"], // Allow only the same origin for other resources
+        "script-src": ["'self'"], // Allow scripts only from the same origin
+        "style-src": ["'self'", "'unsafe-inline'"], // Allow styles from the same origin
+      },
+    },
+  },
+};
